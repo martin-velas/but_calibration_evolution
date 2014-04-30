9a7a8ce5cbc710a4140f5b3a59c0ed43fbed8dc9
ivelas <ivelas@fit.vutbr.cz>
2014-04-30 14:05:41 +0200

    Initialization

---
+++ src/calibration.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <cstdio>
+#include <math.h>
+#include <algorithm>
+
+#include "opencv2/opencv.hpp"
+
+#include <pcl_ros/point_cloud.h>
+#include <boost/foreach.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+#include <velodyne_pointcloud/point_types.h>
+#include <pcl/common/eigen.h>
+#include <pcl/common/transforms.h>
+
+#include <Velodyne.h>
+#include <Calibration.h>
+#include <Image.h>
+
+using namespace cv;
+using namespace std;
+using namespace pcl;
+
+int main(int argc, char** argv)
+{
+  return EXIT_SUCCESS;
+}
